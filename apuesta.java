abf6e16602d015015c1f8e27dd53f406f855d0af
yoyi-8 <yoelito888@gmail.com>
2024-12-13 09:14:31 +0100

    Revisión 3. INICIALES-JavaDoc Apuesta.java

--- apuesta.java
+++ apuesta.java
@@ -9,9 +9,17 @@ import javax.swing.WindowConstants;
  * and open the template in the editor.
  */
 
+/**
+ *
+ * @author 1DAW2425-02
+ */
+
+
 public class Apuesta extends javax.swing.JFrame {
 
-   
+    /**
+     *
+     */
     public Apuesta() 
     {
         initComponents();
@@ -140,7 +148,12 @@ public class Apuesta extends javax.swing.JFrame {
         this.listaResultados.setModel(modelovacio);
     }//GEN-LAST:event_bResetearActionPerformed
 
-   
+    /**
+     *
+     * @param n1
+     * @param n2
+     * @return
+     */
     public int cualquiera(int n1, int n2)
     {
         return (int)Math.floor((Math.random()*(n2-n1+1) + n1));
@@ -172,7 +185,10 @@ public class Apuesta extends javax.swing.JFrame {
         this.listaResultados.setModel(modeloquinielaresuelta);
     }//GEN-LAST:event_bRealizarActionPerformed
 
-    
+    /**
+     *
+     * @param partido
+     */
     public void partidoNuevo(String partido)
     {
         DefaultListModel modelonuevo = new DefaultListModel();
@@ -186,7 +202,10 @@ public class Apuesta extends javax.swing.JFrame {
         this.listaQuiniela.setModel(modelonuevo);
     }
     
-  
+    /**
+     *
+     * @param indice
+     */
     public void borraPartido(int indice)
     {
         DefaultListModel modelonuevoquiniela = new DefaultListModel();
@@ -212,7 +231,10 @@ public class Apuesta extends javax.swing.JFrame {
         this.listaResultados.setModel(modelonuevoresultados);
     }
     
-   
+    /**
+     *
+     * @param args
+     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
